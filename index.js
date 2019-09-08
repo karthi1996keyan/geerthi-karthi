2f50f84b5bfd67a1999f20d14c184ba8caa68d94
Gmonknrteam <geethi.siva48249@gmail.com>
2019-09-09 00:14:39 +0530

    this is second commit

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-alert("karthikeyan");
-alert("karthikeyan");
-alert("karthikeyan");
+var express=require('express');
+var app=express();
+
+app.get('/test',(req,res)=>
+{
+    res.send("this is from jenkins CI/CD pipeline");
+})
+
+app.listen(3000,()=>
+{
+    console.log('Port is running in 3000');
+});
